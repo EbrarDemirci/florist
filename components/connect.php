64d4a65986b330aff30f8b324f9fd4cef53d4bfa
zehracanturk <zehracanturk01@gmail.com>
2025-03-13 14:17:17 +0300

    değiştirme yapıldı

--- components/connect.php
+++ components/connect.php
@@ -1,17 +1,19 @@
-<?php 
+//
+//<?php 
 
-  $db_name = 'mysql:host=localhost;dbname=florist';
-  $user_name = 'root';
-  $user_password ='';
-
-
-  $conn =new PDO($db_name,$user_name, $user_password);
+ // $db_name = 'mysql:host=localhost;dbname=florist';
+  //$user_name = 'root';
+  //$user_password ='';
 
 
-   if($conn){
-   echo "connected";
-
-  }
+ // $conn =new PDO($db_name,$user_name, $user_password);
 
 
-?>
+   //if($conn){
+   //echo "connected";
+
+ // }
+
+
+//?>
+//
