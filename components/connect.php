ccdf762ed6d1d8d19c352c376a0b86f57cfe3770
EbrarDemirci <dmrc.ebrar@gmail.com>
2025-03-17 16:08:16 +0300

    yorumlar kaldırıldı

--- components/connect.php
+++ components/connect.php
@@ -1,19 +1,19 @@
 //
 //<?php 
 
- // $db_name = 'mysql:host=localhost;dbname=florist';
-  //$user_name = 'root';
-  //$user_password ='';
+  $db_name = 'mysql:host=localhost;dbname=florist';
+  $user_name = 'root';
+  $user_password ='';
 
 
- // $conn =new PDO($db_name,$user_name, $user_password);
+  $conn =new PDO($db_name,$user_name, $user_password);
 
 
-   //if($conn){
-   //echo "connected";
+   if($conn){
+   echo "connected";
 
- // }
+   }
 
 
-//?>
+?>
 //
