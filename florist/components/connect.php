9002123dda07f2283b47c423b7c8f7a078f639bf
EbrarDemirci <dmrc.ebrar@gmail.com>
2025-03-19 01:47:13 +0300

    imdat

--- florist/components/connect.php
+++ florist/components/connect.php
@@ -15,4 +15,16 @@
    }
 
 
+   function unique_id(){ 
+    $chars = '0123456789abcdefghijklmnopgrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+    $charsLength = strLen($chars);
+    $randomString = ''; 
+   } 
+    
+   for ($i=0; $i < 20; $i++) { 
+        $randomString.=$chars[mt_rand(0, $charsLength - 1)]; 
+   return $randomString ; 
+   
+} 
+   
 ?>
